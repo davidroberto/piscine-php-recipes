4fec045a59aac33d1c279af029e51a49266e4b8a
David Robert <contact@david-robert.fr>
2025-03-12 16:58:45 +0100

    header partial in view

--- view/index.php
+++ view/index.php
@@ -3,24 +3,9 @@
   require_once('../recipes-repository.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Recettes</title>
-
-  </head>
-  <body>
-
-  <header>
-	<nav>
-		<ul>
-			<li><a href="index.php">Toutes les recettes</a></li>
-		</ul>
-	</nav>
-  </header>
+<?php
+  require_once('partial/header.php');
+?>
 
 	<main>
 
