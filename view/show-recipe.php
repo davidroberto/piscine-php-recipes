83cf71b03f34344519af931bbcca7c11189598e7
David Robert <contact@david-robert.fr>
2025-03-13 12:29:12 +0100

    if index is not in url

--- view/show-recipe.php
+++ view/show-recipe.php
@@ -1,12 +1,6 @@
 <?php
   require_once('../config.php');
   require_once('../recipes-repository.php');
-
-  // je récupère l'info de l'index de la recette cliquée
-  // grâce à la variable $_GET de PHP.
-  // cette variable est fournie par PHP et nous permet de récupérer des infos dans l'url
-  $index = $_GET["index"];
-
 ?>
 
 <?php
@@ -15,25 +9,35 @@
 
 	<main>
 
-		<?php if (!array_key_exists($index, $recipes)) { ?>
+		<?php if (array_key_exists("index", $_GET)) {
 
-			<h2>404 : recette non trouvée</h2>
+			$index = $_GET["index"];
+
+			if (array_key_exists($index, $recipes)) { ?>
+
+				<h2 class="main-title">Détails de la recette</h2>
+
+				<article>
+					<!-- je récupère dans la liste des recettes, uniquement la recette
+					qui correspond à l'index de la recette cliquée
+					-->
+					<h2><?php echo $recipes[$index]["title"]; ?></h2>
+					<img class="recipe-show-image" src="<?php echo $recipes[$index]['image']; ?>" alt="<?php echo $recipe['title']; ?>" />
+					<p><?php echo $recipes[$index]["description"]; ?></p>
+					<p>Publié le : <?php echo $recipes[$index]["publishedDate"]->format('Y-m-d H:i:s'); ?></p>
+				</article>
+
+			<?php } else { ?>
+
+				<h2>404 : recette non trouvée</h2>
+
+			<?php } ?>
 
 		<?php } else { ?>
 
-			<h2 class="main-title">Détails de la recette</h2>
+			<h2>404 : recette non trouvée</h2>
 
-			<article>
-				<!-- je récupère dans la liste des recettes, uniquement la recette
-				qui correspond à l'index de la recette cliquée
-				-->
-				<h2><?php echo $recipes[$index]["title"]; ?></h2>
-				<img class="recipe-show-image" src="<?php echo $recipes[$index]['image']; ?>" alt="<?php echo $recipe['title']; ?>" />
-				<p><?php echo $recipes[$index]["description"]; ?></p>
-				<p>Publié le : <?php echo $recipes[$index]["publishedDate"]->format('Y-m-d H:i:s'); ?></p>
-			</article>
-
-		<?php } ?>
+		<?php } ?>		
 
 	</main>
 
