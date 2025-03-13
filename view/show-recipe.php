dd4c52a111ecfa73892635b9b34534d57c55fcf5
David Robert <contact@david-robert.fr>
2025-03-13 11:47:17 +0100

    handle 404 recipes

--- view/show-recipe.php
+++ view/show-recipe.php
@@ -6,23 +6,34 @@
   // grâce à la variable $_GET de PHP.
   // cette variable est fournie par PHP et nous permet de récupérer des infos dans l'url
   $index = $_GET["index"];
+
 ?>
 
 <?php
   require_once('partial/header.php');
 ?>
 
-	<main >
+	<main>
 
-		<h2 class="main-title">Détails de la recette</h2>
-			<!-- je récupère dans la liste des recettes, uniquement la recette
-			 qui correspond à l'index de la recette cliquée
-			  -->
-			<h2><?php echo $recipes[$index]["title"]; ?></h2>
-			<img class="recipe-show-image" src="<?php echo $recipes[$index]['image']; ?>" alt="<?php echo $recipe['title']; ?>" />
-			<p><?php echo $recipes[$index]["description"]; ?></p>
-			<p>Publié le : <?php echo $recipes[$index]["publishedDate"]->format('Y-m-d H:i:s'); ?></p>
-		</article>
+		<?php if (!array_key_exists($index, $recipes)) { ?>
+
+			<h2>404 : recette non trouvée</h2>
+
+		<?php } else { ?>
+
+			<h2 class="main-title">Détails de la recette</h2>
+
+			<article>
+				<!-- je récupère dans la liste des recettes, uniquement la recette
+				qui correspond à l'index de la recette cliquée
+				-->
+				<h2><?php echo $recipes[$index]["title"]; ?></h2>
+				<img class="recipe-show-image" src="<?php echo $recipes[$index]['image']; ?>" alt="<?php echo $recipe['title']; ?>" />
+				<p><?php echo $recipes[$index]["description"]; ?></p>
+				<p>Publié le : <?php echo $recipes[$index]["publishedDate"]->format('Y-m-d H:i:s'); ?></p>
+			</article>
+
+		<?php } ?>
 
 	</main>
 
